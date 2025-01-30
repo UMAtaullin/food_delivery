eca80fe1627e843e2cf3a604f07a2a9910648796
UMAtaullin <ataullinural@gmail.com>
2025-01-30 11:56:01 +0500

    feat: add order and extends from base.html

--- backend/page.py
+++ backend/page.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template # type: ignore
+from flask import Flask, render_template, request 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('./index.html')
+
+
+@app.route("/order", methods=("GET", "POST"))
+def order():
+    if request.method == "POST":
+        drink = request.form["drink"]
+        print("Drink: ", drink)
+        return render_template("print.html", drink=drink)
+    return render_template("forms.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
